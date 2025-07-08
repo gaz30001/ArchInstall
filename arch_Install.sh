40b5adeaa2321238689366b8a00bcd2cd1a7e204
gaz30001 <51251202+gaz30001@users.noreply.github.com>
2025-07-09 01:54:23 +0300

    Update arch_Install.sh

--- arch_Install.sh
+++ arch_Install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
-# Colors
+# Цвета
 GREEN="\e[32m"
 RED="\e[31m"
 YELLOW="\e[33m"
 BLUE="\e[34m"
 RESET="\e[0m"
 
-set -e
-
+# Приветствие
 clear
 echo -e "${BLUE}"
 cat << "EOF"
@@ -21,57 +21,49 @@ EOF
 echo -e "${RESET}"
 echo -e "${YELLOW}Welcome! Installing Arch Linux with style...${RESET}"
 
-# Confirm to continue
 read -p "Do you want to continue? (y/N): " confirm
-if [[ "$confirm" != "y" ]]; then
-    echo -e "${RED}Aborted.${RESET}"
-    exit 1
-fi
+[[ "$confirm" != "y" ]] && echo -e "${RED}Aborted.${RESET}" && exit 1
 
-# Check internet
-ping -q -c 1 archlinux.org > /dev/null || { echo -e "${RED}No internet connection.${RESET}"; exit 1; }
+# Проверка интернета
+ping -q -c 1 archlinux.org > /dev/null || { echo -e "${RED}No internet.${RESET}"; exit 1; }
 
-# Time setup
 timedatectl set-ntp true
 
-# List available disks
+# Выбор диска
 echo -e "\n${GREEN}Available disks:${RESET}"
 mapfile -t DISKS < <(lsblk -dpno NAME,SIZE | grep -v "boot")
-for i in "${!DISKS[@]}"; do
-    echo "$((i+1))) ${DISKS[$i]}"
-done
+for i in "${!DISKS[@]}"; do echo "$((i+1))) ${DISKS[$i]}"; done
 read -p "Choose disk number to install on: " disk_index
 DISK=$(echo ${DISKS[$((disk_index-1))]} | awk '{print $1}')
 
-# Partition type
+# Тип таблицы
 echo -e "\nPartition type:" 
 echo "1) GPT (UEFI)"
 echo "2) MBR (BIOS)"
 read -p "Choose (1-2): " part_type
 [[ "$part_type" == "1" ]] && PART_TABLE="gpt" || PART_TABLE="mbr"
 
-# Hostname and username
 read -p "Enter hostname [archlinux]: " HOSTNAME
 HOSTNAME=${HOSTNAME:-archlinux}
 read -p "Enter username [user]: " USERNAME
 USERNAME=${USERNAME:-user}
 
-# Passwords
+# Пароли
 while true; do
     read -s -p "Enter root password: " ROOT_PASSWORD; echo
     read -s -p "Confirm root password: " ROOT_PASSWORD2; echo
     [[ "$ROOT_PASSWORD" == "$ROOT_PASSWORD2" ]] && break
-    echo -e "${RED}Passwords don't match!${RESET}"
+    echo -e "${RED}Mismatch.${RESET}"
 done
 
 while true; do
-    read -s -p "Enter password for $USERNAME: " USER_PASSWORD; echo
+    read -s -p "Password for $USERNAME: " USER_PASSWORD; echo
     read -s -p "Confirm password: " USER_PASSWORD2; echo
     [[ "$USER_PASSWORD" == "$USER_PASSWORD2" ]] && break
-    echo -e "${RED}Passwords don't match!${RESET}"
+    echo -e "${RED}Mismatch.${RESET}"
 done
 
-# Select timezone
+# Таймзона
 mapfile -t REGIONS < <(timedatectl list-timezones | cut -d'/' -f1 | sort -u | grep -v -e '^$' -e '^Etc$')
 echo -e "\nChoose region:"
 (for i in "${!REGIONS[@]}"; do printf "%3d) %-15s\n" "$((i+1))" "${REGIONS[$i]}"; done) | column
@@ -84,14 +76,13 @@ echo -e "\nChoose city:"
 read -p "City (1-${#ZONES[@]}): " zone_choice
 TIMEZONE="${ZONES[$((zone_choice-1))]}"
 
-# Locale selection
-echo -e "\nChoose locales (space-separated, e.g., 1 2):"
+# Локали
+echo -e "\nChoose locales (space-separated):"
 LOCALE_LIST=("en_US.UTF-8 UTF-8" "ru_RU.UTF-8 UTF-8" "de_DE.UTF-8 UTF-8")
 (for i in "${!LOCALE_LIST[@]}"; do echo "$((i+1))) ${LOCALE_LIST[$i]}"; done)
 read -p "Locales: " locale_choices
 
-# Format disk
-echo -e "\n${GREEN}Wiping and partitioning $DISK...${RESET}"
+# Разметка
 umount -R /mnt || true
 sgdisk --zap-all $DISK || true
 
@@ -115,24 +106,26 @@ btrfs subvolume create /mnt/@snapshots
 btrfs subvolume create /mnt/@var_log
 umount /mnt
 
-mount -t btrfs -o subvol=@,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt
+BTRFS_OPTS="subvol=@,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2"
+mount -t btrfs -o $BTRFS_OPTS $PART_ROOT /mnt
 mkdir -p /mnt/{boot,home,.snapshots,var/log}
 mount -t btrfs -o subvol=@home,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt/home
 mount -t btrfs -o subvol=@snapshots,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt/.snapshots
 mount -t btrfs -o subvol=@var_log,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt/var/log
 [[ -n "$PART_EFI" ]] && mount $PART_EFI /mnt/boot
 
-# Install base system
-pacstrap -K /mnt base base-devel linux linux-firmware btrfs-progs networkmanager zsh git grub sudo terminus-font xterm pcmanfm ranger feh xorg xorg-xinit mesa xf86-video-vesa
+# Установка базовой системы
+pacstrap -K /mnt base base-devel linux linux-firmware btrfs-progs \
+networkmanager zsh git grub sudo terminus-font xterm pcmanfm ranger \
+feh xorg xorg-xinit mesa xf86-video-vesa bspwm polybar
 
-# fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Chroot and setup
+# Chroot
 arch-chroot /mnt /bin/bash <<EOF
-
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
+setfont ter-v18b
 
 echo "$HOSTNAME" > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -141,22 +134,19 @@ echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 
 EOF
 
-# Set locale
+# Локали
 for choice in $locale_choices; do
     echo "${LOCALE_LIST[$((choice-1))]}" >> /mnt/etc/locale.gen
 done
 arch-chroot /mnt locale-gen
 
-# Устанавливаем первую выбранную локаль как основную
 first_choice=$(echo $locale_choices | awk '{print $1}')
 arch-chroot /mnt bash -c "echo LANG=$(echo ${LOCALE_LIST[$((first_choice-1))]} | cut -d' ' -f1) > /etc/locale.conf"
 
-# Set root password
+# root и пользователь
 echo "root:$ROOT_PASSWORD" | arch-chroot /mnt chpasswd
-
-# Create user
-arch-chroot /mnt useradd -m -G wheel -s /bin/zsh $USERNAME
-echo "${USERNAME}:$USER_PASSWORD" | arch-chroot /mnt chpasswd
+arch-chroot /mnt useradd -m -G wheel,video,audio -s /bin/zsh $USERNAME
+echo "$USERNAME:$USER_PASSWORD" | arch-chroot /mnt chpasswd
 arch-chroot /mnt sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 # Bootloader
@@ -165,10 +155,10 @@ if [[ "$PART_TABLE" == "gpt" ]]; then
   UUID=$(blkid -s UUID -o value $PART_ROOT)
   echo "default arch" > /mnt/boot/loader/loader.conf
   cat <<BOOT > /mnt/boot/loader/entries/arch.conf
-  title   Arch Linux
-  linux   /vmlinuz-linux
-  initrd  /initramfs-linux.img
-  options root=UUID=$UUID rootflags=subvol=@ rw
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=UUID=$UUID rootflags=subvol=@ rw
 BOOT
 else
   arch-chroot /mnt grub-install --target=i386-pc $DISK
@@ -178,17 +168,21 @@ fi
 # Enable services
 arch-chroot /mnt systemctl enable NetworkManager
 
-# Oh-my-zsh and plugins
+# ZSH и yay
 arch-chroot /mnt su - $USERNAME -c "git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh"
 arch-chroot /mnt su - $USERNAME -c "git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 arch-chroot /mnt su - $USERNAME -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 arch-chroot /mnt su - $USERNAME -c "cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc"
-arch-chroot /mnt su - $USERNAME -c "sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc"
 arch-chroot /mnt su - $USERNAME -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"passion\"/' ~/.zshrc"
+arch-chroot /mnt su - $USERNAME -c "sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc"
 
-# yay installer
+# Разрешение sudo без пароля для yay
+echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /mnt/etc/sudoers.d/$USERNAME
+chmod 440 /mnt/etc/sudoers.d/$USERNAME
+
+# Установка yay
 arch-chroot /mnt pacman -S --noconfirm go
-arch-chroot /mnt su - $USERNAME -c "git clone https://aur.archlinux.org/yay.git /home/$USERNAME/yay && cd /home/$USERNAME/yay && makepkg -si --noconfirm"
+arch-chroot /mnt su - $USERNAME -c "cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm"
 
 # Xresources Gruvbox
 cat <<XCONF > /mnt/home/$USERNAME/.Xresources
@@ -213,5 +207,5 @@ cat <<XCONF > /mnt/home/$USERNAME/.Xresources
 XCONF
 arch-chroot /mnt chown $USERNAME:$USERNAME /home/$USERNAME/.Xresources
 
-# Done
+# Завершение
 echo -e "\n${GREEN}Installation complete! You may now reboot.${RESET}"
