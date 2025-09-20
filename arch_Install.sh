774f8abd83d5615a71bc518017fae3ca28449fb2
gaz30001 <51251202+gaz30001@users.noreply.github.com>
2025-09-20 20:08:24 +0300

    Update arch_Install.sh

--- arch_Install.sh
+++ arch_Install.sh
@@ -25,7 +25,7 @@ cat << "EOF"
    \_/\_/  |_|_|\___\___/|_| |_| |_|\___|
 
 EOF
-echo -e "${RESET}${GREEN}Welcome to Arch Linux Automated Installer!${RESET}"
+echo -e "${RESET}${GREEN}Welcome to Arch Linux Automated Installer (Optimized for Older Hardware)!${RESET}"
 
 #-------------------- Подтверждение --------------------
 
@@ -83,6 +83,7 @@ done
 
 #-------------------- Выбор таймзоны --------------------
 
+# Этот раздел оставлен без изменений, так как он не влияет на производительность
 mapfile -t REGIONS < <(timedatectl list-timezones | cut -d'/' -f1 | sort -u | grep -v -e '^$' -e '^Etc$')
 echo -e "\nChoose region:"
 (for i in "${!REGIONS[@]}"; do printf "%3d) %-15s\n" "$((i+1))" "${REGIONS[$i]}"; done) | column
@@ -100,40 +101,44 @@ TIMEZONE="${ZONES[$((zone_choice-1))]}"
 echo -e "\nChoose locales (space-separated):"
 LOCALE_LIST=("en_US.UTF-8 UTF-8" "ru_RU.UTF-8 UTF-8")
 (for i in "${!LOCALE_LIST[@]}"; do echo "$((i+1))) ${LOCALE_LIST[$i]}"; done)
-read -p "Locales: " locale_choices
+read -p "Locales [1 2]: " locale_choices
+locale_choices=${locale_choices:-"1 2"}
 
-#-------------------- Разметка --------------------
+#-------------------- Разметка диска (оптимизировано под Ext4) --------------------
 
 umount -R /mnt || true
 sgdisk --zap-all $DISK || true
 
 if [[ "$PART_TABLE" == "gpt" ]]; then
-    sgdisk -n 1:0:+550M -t 1:ef00 $DISK
-    sgdisk -n 2:0:0 -t 2:8300 $DISK
-    PART_EFI="${DISK}1"; PART_ROOT="${DISK}2"
+    # GPT for UEFI
+    sgdisk -n 1:0:+550M -t 1:ef00 $DISK   # EFI partition
+    sgdisk -n 2:0:0 -t 2:8300 $DISK      # Linux root
+    PART_EFI="${DISK}p1" # Используем суффикс 'p' для GPT, если диск nvme, то будет nvme0n1p1
+    PART_ROOT="${DISK}p2"
     mkfs.fat -F32 $PART_EFI
 else
+    # MBR for BIOS
     parted -s $DISK mklabel msdos
     parted -s $DISK mkpart primary ext4 1MiB 100%
     parted -s $DISK set 1 boot on
     PART_ROOT="${DISK}1"
 fi
 
-mkfs.btrfs -f -L ArchLinux $PART_ROOT
-mount -t btrfs $PART_ROOT /mnt
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@snapshots
-btrfs subvolume create /mnt/@var_log
-umount /mnt
+echo -e "\n${YELLOW}Formatting root partition with Ext4 (fast and reliable for old hardware)...${RESET}"
+mkfs.ext4 -F -L ArchLinux $PART_ROOT
+mount $PART_ROOT /mnt
+if [[ -n "$PART_EFI" ]]; then
+    mkdir -p /mnt/boot
+    mount $PART_EFI /mnt/boot
+fi
 
-BTRFS_OPTS="subvol=@,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2"
-mount -t btrfs -o $BTRFS_OPTS $PART_ROOT /mnt
-mkdir -p /mnt/{boot,home,.snapshots,var/log}
-mount -t btrfs -o subvol=@home,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt/home
-mount -t btrfs -o subvol=@snapshots,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt/.snapshots
-mount -t btrfs -o subvol=@var_log,noatime,compress=zstd:2,ssd,discard=async,space_cache=v2 $PART_ROOT /mnt/var/log
-[[ -n "$PART_EFI" ]] && mount $PART_EFI /mnt/boot
+#-------------------- Создание файла подкачки (Swap) --------------------
+read -p "Enter swap file size in MB (e.g., 1024 for 1GB) [1024]: " SWAP_SIZE
+SWAP_SIZE=${SWAP_SIZE:-1024}
+dd if=/dev/zero of=/mnt/swapfile bs=1M count=$SWAP_SIZE status=progress
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
 
 #-------------------- Обновление зеркал --------------------
 
@@ -141,21 +146,28 @@ echo -e "\n${GREEN}Updating mirror list for faster downloads...${RESET}"
 pacman -Sy --noconfirm reflector
 reflector --verbose --country Russia,Germany --sort rate -n 10 --save /etc/pacman.d/mirrorlist
 
-#-------------------- Установка системы --------------------
+#-------------------- Установка системы (оптимизированный набор пакетов) --------------------
 
 echo -e "\n${GREEN}Installing base system (pacstrap)... This may take a while.${RESET}"
-pacstrap -K /mnt base base-devel linux-zen linux-firmware btrfs-progs \
-networkmanager zsh git grub sudo terminus-font xterm pcmanfm ranger \
-feh xorg xorg-xinit mesa xf86-video-vesa bspwm polybar neovim go
+# - linux-lts: ядро с долгосрочной поддержкой, лучше для старого железа
+# - xf86-video-ati: драйвер для вашей видеокарты ATI Radeon X1250
+# - openbox, tint2: легковесный оконный менеджер и панель
+# - efibootmgr: нужен для UEFI загрузчика
+# - Удалены: linux-zen, btrfs-progs, xf86-video-vesa, bspwm, polybar, go
+pacstrap -K /mnt base base-devel linux-lts linux-firmware \
+networkmanager dhcpcd zsh git sudo terminus-font xterm pcmanfm ranger feh \
+xorg xorg-xinit mesa xf86-video-ati openbox tint2 efibootmgr
 
 genfstab -U /mnt >> /mnt/etc/fstab
+# Убедимся, что swap есть в fstab
+echo "/swapfile none swap defaults 0 0" >> /mnt/etc/fstab
 
 #-------------------- Настройки в chroot --------------------
 
 arch-chroot /mnt /bin/bash <<EOF
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
-setfont ter-v18b
+setfont ter-v18n # Шрифт Terminus для кириллицы
 
 echo "$HOSTNAME" > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -184,14 +196,17 @@ arch-chroot /mnt sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/
 if [[ "$PART_TABLE" == "gpt" ]]; then
     arch-chroot /mnt bootctl --path=/boot install
     UUID=$(blkid -s UUID -o value $PART_ROOT)
-    echo "default arch" > /mnt/boot/loader/loader.conf
-    cat <<BOOT > /mnt/boot/loader/entries/arch.conf
-title   Arch Linux
-linux   /vmlinuz-linux-zen
-initrd  /initramfs-linux-zen.img
-options root=UUID=$UUID rootflags=subvol=@ rw
+    echo "default arch-lts" > /mnt/boot/loader/loader.conf
+    echo "timeout 3" >> /mnt/boot/loader/loader.conf
+    cat <<BOOT > /mnt/boot/loader/entries/arch-lts.conf
+title   Arch Linux (LTS)
+linux   /vmlinuz-linux-lts
+initrd  /initramfs-linux-lts.img
+options root=UUID=$UUID rw
 BOOT
 else
+    # Добавляем grub в pacstrap, если его там еще нет
+    pacman -S --noconfirm grub
     arch-chroot /mnt grub-install --target=i386-pc $DISK
     arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 fi
@@ -209,12 +224,26 @@ arch-chroot /mnt su - $USERNAME -c "cp ~/.oh-my-zsh/templates/zshrc.zsh-template
 arch-chroot /mnt su - $USERNAME -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"passion\"/' ~/.zshrc"
 arch-chroot /mnt su - $USERNAME -c "sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc"
 
-# Разрешение sudo без пароля для yay
-echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/bin/pacman" > /mnt/etc/sudoers.d/$USERNAME
-chmod 440 /mnt/etc/sudoers.d/$USERNAME
-
-# Установка yay
+# Установка yay (без небезопасных правил sudo)
 arch-chroot /mnt su - $USERNAME -c "cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm"
+
+#-------------------- Настройка графической сессии (Openbox) --------------------
+
+# Создаем базовый .xinitrc для запуска Openbox
+cat <<XINIT > /mnt/home/$USERNAME/.xinitrc
+#!/bin/sh
+# Загрузка ресурсов X (цвета и т.д.)
+xrdb -merge ~/.Xresources &
+# Запуск панели
+tint2 &
+# Запуск оконного менеджера Openbox
+exec openbox-session
+XINIT
+
+# Копируем конфиги Openbox и Tint2 по умолчанию
+arch-chroot /mnt su - $USERNAME -c "mkdir -p ~/.config/openbox && cp -r /etc/xdg/openbox/* ~/.config/openbox/"
+arch-chroot /mnt su - $USERNAME -c "mkdir -p ~/.config/tint2 && cp /etc/xdg/tint2/tint2rc ~/.config/tint2/"
+arch-chroot /mnt chown -R $USERNAME:$USERNAME /home/$USERNAME
 
 #-------------------- Xresources Gruvbox --------------------
 
@@ -240,7 +269,9 @@ cat <<XCONF > /mnt/home/$USERNAME/.Xresources
 XCONF
 arch-chroot /mnt chown $USERNAME:$USERNAME /home/$USERNAME/.Xresources
 
+
 #-------------------- Конец --------------------
 
-echo -e "\n${GREEN}Installation complete! You may now reboot.${RESET}"
-
+echo -e "\n${GREEN}Installation complete!${RESET}"
+echo -e "${YELLOW}After reboot, log in as '$USERNAME' and run the 'startx' command to start the graphical environment.${RESET}"
+echo -e "${YELLOW}You may now reboot.${RESET}"
